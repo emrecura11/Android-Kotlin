50352a486c64b314c58795a85b598d3fe69dcfa5
Emre Cura <emre11cura@gmail.com>
2024-11-20 17:39:26 +0300

    Navigation

---
+++ Navigation/app/src/main/java/com/hadrianstudios/navigation/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.hadrianstudios.navigation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navigationController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        navigationController = Navigation.findNavController(this, R.id.fragment)
+       // NavigationUI.setupActionBarWithNavController(this,navigationController)
+
+    }
+
+    //geri oku çıkartır
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navigationController,null)
+    }
+}
---
+++ Navigation/app/src/main/java/com/hadrianstudios/navigation/SecondFragment.kt
@@ -0,0 +1,37 @@
+package com.hadrianstudios.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+
+class SecondFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button : Button = view.findViewById(R.id.secondFragmentButton)
+        button.setOnClickListener {
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
